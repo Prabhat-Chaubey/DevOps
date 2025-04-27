aed4cffd932318325e315840ae4799fa6a4b9ddf
Prabhat Chaubey <118340523+Prabhat-Chaubey@users.noreply.github.com>
2025-04-28 02:11:45 +0530

    Create Condition.sh

---
+++ BashScripting/Condition.sh
@@ -0,0 +1,23 @@
+#String COmaprator :
+    #==        : Check is string are equla
+    #!=        : Checks if string are not equa;
+    #-z "$str" : checks if string is empty
+    #-n "$str" : checks if string is not empty
+
+# Integer Comarator:
+    #-lt : less than
+    #-gt : Greater than
+    #-ne : not equal
+    #-eq : equal
+    
+#!/bin/bash
+
+echo "Enter a number "
+read number
+
+if [ $number -gt 10 ] ;
+  then echo "Hello"
+else
+  echo "Bhago"
+
+  
